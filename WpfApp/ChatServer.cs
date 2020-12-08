8312251e5958a899e7a5d306923ed0cc373a4713
DaanBakker <daanbakker1995@users.noreply.github.com>
2020-12-08 15:41:42 +0100

    Add files via upload

---
+++ WpfApp/ChatServer.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppServer
+{
+    class ChatServer
+    {
+        // Settings for server
+        public int PORTNR { get; }
+        public int BUFFERSIZE { get; }
+        public string IPADRESS { get; }
+        private string EndOfTransitionCharacter = "@-_@";
+
+        // TcpListener for listening for clients
+        private TcpListener TcpListener = null;
+
+        // List of TcpCLients
+        private List<TcpClient> TcpClients = new List<TcpClient>();
+
+        // boolean holding the Server Status
+        private bool ServerStarted { get; set; }
+
+        // Delegates/Actions to update UI
+        private Action<string> AddMessageToChat;
+        private Action ToggleStartButton;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="PortNr"></param>
+        /// <param name="BuffferSize"></param>
+        /// <param name="IPAdress"></param>
+        /// <param name="AddMessageToChat"></param>
+        /// <param name="ToggleStartButton"></param>
+        public ChatServer(int PortNr, int BuffferSize, String IPAdress, Action<String> AddMessageToChat, Action ToggleStartButton)
+        {
+            this.PORTNR = PortNr;
+            this.BUFFERSIZE = BuffferSize;
+            this.IPADRESS = IPAdress;
+            this.AddMessageToChat = AddMessageToChat;
+            this.ToggleStartButton = ToggleStartButton;
+
+            ServerStarted = false;
+            TcpClients = new List<TcpClient>();
+        }
+        
+        public void StartListening()
+        {
+            try
+            {
+                ServerStarted = true;
+                TcpListener = new TcpListener(IPAddress.Parse(IPADRESS), PORTNR);
+                TcpListener.Start();
+                AddMessageToChat("Luisteren naar chatclients...");
+                TcpListener.BeginAcceptTcpClient(AcceptClients, TcpListener);
+            } catch(Exception e)
+            {
+                AddMessageToChat($"Server Error: {e.Message}");
+
+            }
+        }
+
+        private async void AcceptClients(IAsyncResult result)
+        {
+            // Do someting only if server is started.
+            if (ServerStarted)
+            {
+                // Use the returned TCP client from the TcpListener
+                using (TcpClient tcpClient = TcpListener.EndAcceptTcpClient(result))
+                {
+                    try
+                    {
+                        // Check if server is not closed
+                        if (ServerStarted)
+                        {
+                            // Add Client to list of clients
+                            TcpClients.Add(tcpClient);
+                            // Listen for new client to connect
+                            TcpListener.BeginAcceptTcpClient(AcceptClients, TcpListener);
+                            // Receive data on net Taks
+                            await Task.Run(() => ReceiveData(tcpClient));
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        // Add exception to chat if server started and an exception caught
+                        if (ServerStarted) AddMessageToChat("AcceptClients error:" + e.Message);
+                    }
+                }
+            }
+        }
+
+        private void ReceiveData(TcpClient tcpClient)
+        {
+            // Using streamwriter from the TCP Client
+            using (NetworkStream networkStream = tcpClient.GetStream())
+            {
+                try
+                { 
+                    // Send feedback to server UI
+                    AddMessageToChat("Nieuwe chat deelnemer!");
+                    while (networkStream != null && networkStream.CanRead)
+                    {
+                        // Receive data from stream
+                        byte[] byteArray = new byte[BUFFERSIZE];
+                        int resultSize = networkStream.Read(byteArray, 0, BUFFERSIZE);
+                        string message = Encoding.ASCII.GetString(byteArray, 0, resultSize);
+                        
+                        // Make one message from received bytes
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.Append(message);
+                        
+                        //end of Message
+                        if (message.EndsWith(EndOfTransitionCharacter))
+                        {
+                            // Make message readable
+                            string clientMessage = stringBuilder.ToString(); 
+                            clientMessage = clientMessage.Remove(clientMessage.Length - EndOfTransitionCharacter.Length);
+                            if (clientMessage == "bye")
+                                break;
+                            // Display message in chat
+                            AddMessageToChat(clientMessage);
+                            // Send message to other connected clients
+                            BroadCast(clientMessage, tcpClient);
+                            // Empty stringBuilder for new message
+                            stringBuilder = new StringBuilder();
+                        }
+                    }
+                    if (networkStream.CanRead)
+                    {
+                        RemoveClient(tcpClient);
+                    }
+                }
+                catch(Exception e)
+                {
+                    if (networkStream.CanRead) {
+                        // Send error to chat and remove client
+                        AddMessageToChat($"Fout bij ontvangen bericht(en)");
+                        RemoveClient(tcpClient);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Send a message to all connected clients
+        /// </summary>
+        /// <param name="clientMessage"></param>
+        /// <param name="tcpClient"></param>
+        public void BroadCast(string clientMessage, TcpClient tcpClient)
+        {
+            // Make message
+            clientMessage += EndOfTransitionCharacter;
+            // Send message to all connected clients that are not the tcpClient
+            foreach (TcpClient client in TcpClients.Where(client => client != tcpClient))
+            {
+                var networkStream = client.GetStream();
+                var buffer = Encoding.ASCII.GetBytes(clientMessage);
+                networkStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        /// <summary>
+        /// End connection with a specific client
+        /// </summary>
+        /// <param name="client"></param>
+        private void RemoveClient(TcpClient client)
+        {
+            AddMessageToChat("Client heeft chat verlaten");
+            // Close client and remove from List
+            client.Close();
+            TcpClients.Remove(client);
+        }
+
+        /// <summary>
+        /// Stop TcpListener, reset client list and server status
+        /// </summary>
+        public void CloseServer()
+        {
+
+            // Send bye to clients and stop the listener
+            ServerStarted = false;
+            BroadCast("bye",null);
+            TcpListener.Stop();
+            // Reset list
+            TcpClients = new List<TcpClient>();
+            // Update UI
+            ToggleStartButton();
+            AddMessageToChat("Verbinding gesloten..");
+        }
+    }
+}
---
+++ WpfApp/ServerApp.xaml.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WpfAppServer;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ServerApp : Window
+    {
+        ChatServer Server;
+        // Strings for butten UI
+        private const String START_SERVER_TEXT = "Starten";
+        private const String CLOSE_SERVER_TEXT = "Sluiten";
+
+        // Server settings
+        private bool ServerStarted;
+
+        public ServerApp()
+        {
+            InitializeComponent();
+            BtnStartServer.Content = START_SERVER_TEXT;
+            ServerStarted = false;
+        }
+
+        /// <summary>
+        /// <c>Event_Handler</c> BtnStartServer
+        /// </summary>
+        private void BtnStartServer_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ServerStarted)
+            {
+                if (IsValidIP(InputServerIP.Text)
+                    && IsPortNrValid(InputPortNumber.Text)
+                    && IsValidBufferSize(InputBufferSize.Text))
+                {
+                    int portNr = int.Parse(InputPortNumber.Text);
+                    int BufferSize = int.Parse(InputBufferSize.Text);
+                    AddToChatList("Server Starten...");
+                    AddToChatList("Druk op 'Sluiten' of verzend 'bye' om connectie te sluiten");
+                    Server = new ChatServer(portNr, BufferSize, InputServerIP.Text,
+                        (message) => AddToChatList(message), () => UpdateBtnServerStart());
+                    ServerStarted = true;
+                    UpdateBtnServerStart();
+                    Server.StartListening();
+                }
+                else
+                {
+                    UpdateErrorDisplay("Foute gegevens, controleer probeer opnieuw");
+                }
+            }
+            else
+            {
+                CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// <c>Event_Handler</c> BtnSendMessage Click
+        /// </summary>
+        private void BtnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputMessage.Text != "")
+            {
+                if (ServerStarted)
+                {
+                    String message = InputMessage.Text;
+                    UpdateErrorDisplay();
+                    AddToChatList(message);
+                    Server.BroadCast(message, null);
+                    if(message == "bye")
+                    {
+                        CloseConnection();
+                    }
+                    // Empty message field
+                    InputMessage.Clear();
+                    InputMessage.Focus();
+                }
+                else
+                {
+                    UpdateErrorDisplay("Start eerst de server!");
+                }
+            }
+            else
+            {
+                UpdateErrorDisplay();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            AddToChatList("Verbinding sluiten...");
+            // Don't switch these around this wil break Interface
+            ServerStarted = false;
+            Server.CloseServer();
+        }
+
+        /// <summary>
+        /// Adds a message to the chatlist
+        /// </summary>
+        private void AddToChatList(String message)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                ListBoxItem item = new ListBoxItem { Content = message };
+                ChatList.Items.Add(item);
+                // Scroll to item
+                ChatList.ScrollIntoView(item);
+            });
+        }
+
+        /// <summary>
+        /// Update interface with Error, by default empty
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        private void UpdateErrorDisplay(String errorMessage = "")
+        {
+            ErrorTextBlock.Text = errorMessage;
+        }
+
+        /// <summary>
+        /// Update the text of BtnStartServer
+        /// </summary>
+        private void UpdateBtnServerStart()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (!ServerStarted)
+                {
+                    BtnStartServer.Content = START_SERVER_TEXT;
+                }
+                else
+                {
+                    BtnStartServer.Content = CLOSE_SERVER_TEXT;
+                }
+            });
+        }
+       
+        /// <summary>
+        /// Event handler for InputServerIP_KeyUp, checks if valid input else update error display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InputServerIP_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!IsValidIP(InputServerIP.Text))
+            {
+                UpdateErrorDisplay("Ongeldig IP adres.");
+            }
+            else
+            {
+                UpdateErrorDisplay();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for InportPortNumber_KeyUp, checks if input is valid else updates error display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InputPortNumber_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (IsPortNrValid(InputPortNumber.Text))
+            {
+                UpdateErrorDisplay();
+            }
+            else
+            {
+                UpdateErrorDisplay("Ongeldig poort nummer.");
+            }
+        }
+
+        /// <summary>
+        /// Event handler for InputBufferSize_KeyUp, checks if bufferSize is valid else updates error display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InputBufferSize_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (IsValidBufferSize(InputBufferSize.Text))
+            {
+                UpdateErrorDisplay();
+            }
+            else
+            {
+                UpdateErrorDisplay("Ongeldig poort nummer.");
+            }
+        }
+
+        /// <summary>
+        /// Checks if given string can be converted into an int and is its a valid number based on <code>IsValidPortNumber</code>
+        /// </summary>
+        /// <param name="portNr"></param>
+        /// <returns></returns>
+        private bool IsPortNrValid(String portNr)
+        {
+            bool Isvalid = false;
+            if (portNr != "")
+            {
+                if (int.TryParse(portNr, out _))
+                {
+                    if (IsValidPortNumber(int.Parse(portNr)))
+                    {
+                        Isvalid = true;
+                    }
+                }
+            }
+            return Isvalid;
+        }
+
+        /// <summary>
+        /// Check if param is valid ip4
+        /// </summary>
+        /// <param name="IPAdress"></param>
+        /// <returns></returns>
+        private bool IsValidIP(String IPAdress)
+        {
+            const int allowedSplitValues = 4;
+            var splitValues = IPAdress.Split('.');
+            if (splitValues.Length != allowedSplitValues || string.IsNullOrWhiteSpace(IPAdress)) return false; // Check if split value is 4.
+            return splitValues.All(r => byte.TryParse(r, out byte tempForParsing)); // Returns true if all split values can be parsed to bytes
+        }
+
+        /// <summary>
+        /// Check if int is a valid port number.
+        /// Based on valid port numbers: https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Dynamic,_private_or_ephemeral_ports
+        /// </summary>
+        /// <param name="portNumber"></param>
+        /// <returns></returns>
+        private bool IsValidPortNumber(int portNumber)
+        {
+            const int minValidPortNumber = 49152;
+            const int maxValidPortNumber = 65535;
+            if (portNumber >= minValidPortNumber && portNumber <= maxValidPortNumber)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsValidBufferSize(string BufferSize)
+        {
+            Regex RegMatch = new Regex(@"^[0-9]*$");
+            // buffersize can be between 0 and 1024, and only contains numbers -> if valid, field is valid
+            if (RegMatch.IsMatch(BufferSize) && !string.IsNullOrEmpty(BufferSize) &&
+               int.Parse(BufferSize) <= 1024 && int.Parse(BufferSize) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
